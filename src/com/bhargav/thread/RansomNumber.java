2bab5eb24b6d5adf9612fbc7910693ae5977f8a2
bhargav <bhargav@Bhargav-pc>
2017-05-17 00:41:11 -0500

    deleted unused arraylist in ransomnumber

--- src/com/bhargav/thread/RansomNumber.java
+++ src/com/bhargav/thread/RansomNumber.java
@@ -6,24 +6,21 @@ import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class RansomNumber {
-//	List<Integer> rsnum= new ArrayList<Integer>();
-	CopyOnWriteArrayList<Integer> cowal= new CopyOnWriteArrayList<>();
-	
-	synchronized public void mul(int n)
-	{
-		for(int i=1;i<100;i++)
-		{
-			cowal.add(i*n);
-			
+	CopyOnWriteArrayList<Integer> cowal = new CopyOnWriteArrayList<>();
+
+	synchronized public void mul(int n) {
+		for (int i = 1; i < 100; i++) {
+			cowal.add(i * n);
+
 		}
-		
+
 	}
-	
-	synchronized public  void printAll() {
+
+	synchronized public void printAll() {
 		ArrayList<Integer> temp = new ArrayList<>();
 		temp.addAll(cowal);
 		Collections.sort(temp);
-		
+
 		cowal.clear();
 		cowal.addAll(temp);
 		for (Iterator iterator = cowal.iterator(); iterator.hasNext();) {
